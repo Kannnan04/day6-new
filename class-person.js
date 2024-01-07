bc20eb2782ab938254d8074424fdc5053343dafa
Kannnan04 <kannansiva0910@gmail.com>
2024-01-07 16:50:50 +0530

    person task

---
+++ class-person.js
@@ -0,0 +1,13 @@
+class person{
+    constructor(name, course, place, sex, birth){
+        this.name = name;
+        this.course = course;
+        this.place = place;
+        this.sex = sex;
+        this.year = birth;
+    }
+}
+
+let data = new person("kannan", "BE", "neyveli", "male",2001 );
+console.log(data)
+
